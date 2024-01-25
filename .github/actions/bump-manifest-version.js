10912a13bdd74db6e43bcddd2a2800c495e298dd
Gellipapa <gellerthorvath0@gmail.com>
2024-01-25 19:11:22 +0100

    Update bump-manifest-version.js

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -1,10 +1,58 @@
 const fs = require("fs");
+const path = require('path');
+const replace = require('replace-in-file');
+
 
 const version = process.env.TGT_RELEASE_VERSION;
 const newVersion = version.replace("v", "");
+const defaultFolderPath = process.env.GITHUB_WORKSPACE
+const targetFile = 'fxmanifest.lua';
+let allResults = []
 
-const manifestFile = fs.readFileSync("fxmanifest.lua", { encoding: "utf8" });
+function replaceInFiles(folderPath) {
+  fs.readdir(folderPath, { withFileTypes: true }, (err, entries) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
 
-const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`);
+    entries.forEach(entry => {
+      const entryPath = path.join(folderPath, entry.name);
+      
+      if (entry.isDirectory()) {
+        replaceInFiles(entryPath);
+      } else if (entry.isFile() && entry.name === targetFile) {
+        const options = {
+          files: entryPath,
+          from: /\bversion\s+(.*)$/gm,
+          to: `version '${newVersion}'`,
+        };
 
-fs.writeFileSync("fxmanifest.lua", newFileContent);
+        try {
+          const results = replace.sync(options);
+          results.forEach(result => {
+            if (result.hasChanged) {
+              allResults.push(result);
+            }
+          });
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    });
+  });
+}
+
+replaceInFiles(defaultFolderPath)
+
+process.on('exit', () => {
+
+  const changedFiles = allResults.map(result => result.file);
+
+  if (changedFiles.length > 0) {
+    console.log('::set-output name=modified_files::' + JSON.stringify(changedFiles));
+  } else {
+    console.log('No changes!');
+  }
+});
+
