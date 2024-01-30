0cbf3804f7c8928a6e403c7041d4a19e16cbbc1e
Gellipapa <gellerthorvath0@gmail.com>
2024-01-30 02:04:49 +0100

    Update bump-manifest-version.js

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -8,41 +8,32 @@ const defaultFolderPath = process.env.GITHUB_WORKSPACE;
 const targetFile = 'fxmanifest.lua';
 const allResults = [];
 
-const containsBrackets = (name) => {
-  return name.includes('[') && name.includes(']');
-};
-
 const replaceInFiles = (folderPath) => {
-  fs.readdir(folderPath, { withFileTypes: true }, (err, entries) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+  const entries = fs.readdirSync(folderPath, { withFileTypes: true });
 
-    entries.forEach(entry => {
-      const entryPath = path.join(folderPath, entry.name);
-      
-      if (entry.isDirectory() && containsBrackets(entry.name)) {
-        replaceInFiles(entryPath);
-      } else if (entry.isFile() && entry.name === targetFile) {
-        const options = {
-          files: entryPath,
-          from: /\bversion\s+(.*)$/gm,
-          to: `version '${newVersion}'`,
-        };
+  entries.forEach(entry => {
+    const entryPath = path.join(folderPath, entry.name);
+    
+    if (entry.isDirectory()) {
+      replaceInFiles(entryPath);
+    } else if (entry.isFile() && entry.name === targetFile) {
+      const options = {
+        files: entryPath,
+        from: /\bversion\s+(.*)$/gm,
+        to: `version '${newVersion}'`,
+      };
 
-        try {
-          const results = replace.sync(options);
-          results.forEach(result => {
-            if (result.hasChanged) {
-              allResults.push(result);
-            }
-          });
-        } catch (error) {
-          console.error(error);
-        }
+      try {
+        const results = replace.sync(options);
+        results.forEach(result => {
+          if (result.hasChanged) {
+            allResults.push(result);
+          }
+        });
+      } catch (error) {
+        console.error('Error occurred:', error);
       }
-    });
+    }
   });
 };
 
@@ -50,8 +41,8 @@ replaceInFiles(defaultFolderPath);
 
 process.on('exit', () => {
   const changedFiles = allResults.map(result => result.file);
-  console.log(changedFiles)
-  console.log(defaultFolderPath)
+  console.log(changedFiles);
+  console.log(defaultFolderPath);
   if (changedFiles.length > 0) {
     console.log(`echo "FXMANIFEST_FILE_CHANGES=${JSON.stringify(changedFiles)}" >> $GITHUB_ENV`);
   } else {
